404a548aebc19797d4e4a5ea705e2d4790388479
lautaro valle <lvalle@indec.gob.ar>
2023-04-11 18:48:18 -0300

    add delete button

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using MvcMovie.Services;
+
+namespace MvcMovie.Controllers;
+
+public class MovieController : Controller
+{
+    public MovieController()
+    {
+    }
+
+    public IActionResult Index()
+    {   
+        var model = new List<Movie>();
+        model = MovieService.GetAll();
+
+        return View(model);
+    }
+
+    public IActionResult Detail(string id)
+    {
+        var model = MovieService.Get(id);
+
+        return View(model);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Movie movie){
+        if(!ModelState.IsValid){
+            return RedirectToAction("Create");
+        }
+
+        MovieService.Add(movie);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Delete(){
+        return RedirectToAction("Index");
+    }
+}
